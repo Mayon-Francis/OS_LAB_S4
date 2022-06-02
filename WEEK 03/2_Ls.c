ef8310f791a1255a7cbc356812cbbb15edb7f3df
mayon@mec <mayonfrancis@gmail.com>
2022-06-02 11:31:11 +0530

    3_grep

---
+++ WEEK 03/2_Ls.c
@@ -0,0 +1,20 @@
+// gcc 2_Ls.c && ./a.out .
+// gcc 2_Ls.c && ./a.out testDir
+
+
+#include <stdio.h>
+#include <dirent.h>
+
+
+int main(int argc, char *argv[])
+{
+	struct dirent *link;
+	DIR *dp;
+	dp = opendir(argv[1]);
+	printf("Reading directory: %s \n", argv[1]);
+    while ((link = readdir(dp)) != 0)
+    {
+        printf("% s\n", link->d_name);
+    }
+    closedir(dp);
+}
